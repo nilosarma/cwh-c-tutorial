2de9205ffd9807d3209efad24cf3d193bedb2e1e
Nilo Sarma <nilosarma8@gmail.com>
2025-12-23 20:12:58 +0530

    add call by ref code

---
+++ tutorial31.c
@@ -0,0 +1,19 @@
+//Actual and formal parameters
+//types of funtion function call by value and call by reference
+#include<stdio.h>
+/*function definition to swap the values*/
+void swap(int *x, int *y) {
+    int temp;
+    temp = *x; /*save the value at address x*/
+    *x = *y; /*put y into x*/
+    *y = temp; /*put temp into y*/
+    return;
+}
+int main()
+{
+    int a = 34, b = 74;
+    printf("%d and %d\n",a, b);
+    swap(&a, &b);
+    printf("%d and %d\n",a,b);
+    return 0;
+}
