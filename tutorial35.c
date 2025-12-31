bf9cf913e14e031ab7ead4fc0c3bc53fb588fbbd
Nilo Sarma <nilosarma8@gmail.com>
2025-12-31 19:41:35 +0530

    tutorial35.c added

---
+++ tutorial35.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    char s1[] = "harry";
+    char s2[] = "ravi";
+    char s3[54];
+    // puts(strcat(s1, s2));
+    printf("The length of s1 is %d\n", strlen(s1));
+     printf("The length of s2 is %d\n", strlen(s2));
+     printf("The reversed string s1 is: ");
+    puts(strrev(s1));
+      printf("The reversed string s2 is: ");
+    puts(strrev(s2));
+
+    strcpy(s2,s3);
+
+    return 0;
+}
